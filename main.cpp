148663c64b2a596b713b68c4bf67449e1182c8b0
Wojciech Kosierkiewicz <wojtekkosierkiewicz@gmail.com>
2025-03-21 13:42:27 +0100

    filozof gotowy

--- main.cpp
+++ main.cpp
@@ -1,25 +1,93 @@
 #include <iostream>
+#include <mutex>
+#include <queue>
+#include <thread>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+class Restaurant {
+    std::mutex* forks;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    Restaurant(int forksCount) {
+        forks = new std::mutex[forksCount];
+    }
+};
+
+class Waiter {
+    std::queue<int> waitingQueue;
+    Restaurant* restaurant;
+    std::mutex* waiterMutex;
+    Waiter(Restaurant* restaurant) {
+        this->restaurant = restaurant;
+        waiterMutex = new std::mutex();
+    }
+    bool Decide(int id) {
+    }
+};
+
+class Philosopher {
+    int id;
+    Restaurant* restaurant;
+    Waiter* waiter;
+    bool eating;
+    std::mutex *left_fork;
+    std::mutex *right_fork;
+    std::mutex hungryMutex;
+    int hungryCount;
+    int sleepsec;
+
+    Philosopher(int id, Restaurant* restaurant, Waiter* waiter, std::mutex left_fork, std::mutex right_fork,int sleepsec) {
+        this->id = id;
+        this->restaurant = restaurant;
+        this->waiter = waiter;
+        this->left_fork = &left_fork;
+        this->right_fork = &right_fork;
+        this->sleepsec = sleepsec;
+        eating = false;
     }
 
-    return 0;
-}
+    void Eat() {
+        if (!left_fork->try_lock()) {
+            std::cout <<"Philosopher " << id << "Tried to eat without left fork availible" << std::endl;
+            return;
+        }
+        if (!right_fork->try_lock()) {
+            std::cout <<"Philosopher " << id << "Tried to eat without right fork availible" << std::endl;
+            left_fork->unlock();
+            return;
+        }
+        eating = true;
+        std::this_thread::sleep_for(std::chrono::seconds(sleepsec));
+        hungryMutex.lock();
+        hungryCount = 0;
+        hungryMutex.unlock();
+        eating = false;
+        left_fork->unlock();
+        right_fork->unlock();
+    }
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+    void think() {
+        std::this_thread::sleep_for(std::chrono::seconds(sleepsec/10));
+        hungryMutex.lock();
+        hungryCount++;
+        hungryMutex.unlock();
+    }
+
+    int check_hunger() {
+        hungryMutex.lock();
+        int count = hungryCount;
+        hungryMutex.unlock();
+        return count;
+    }
+
+    void try_eating() {
+
+    }
+
+    void Run() {
+        try_eating();
+        think();
+    }
+};
+
+int main() {
+
+}
