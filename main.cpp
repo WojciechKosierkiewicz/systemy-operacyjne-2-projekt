4253d8c5d0f6fc828b5324e2d50ae9ae0c1bc604
Wojciech Kosierkiewicz <wojtekkosierkiewicz@gmail.com>
2025-03-23 17:32:54 +0100

    Problem prawie rozwiązany tylko naprawić segfault w filozofie podczas aktualizacji glodu

--- main.cpp
+++ main.cpp
@@ -3,91 +3,73 @@
 #include <queue>
 #include <thread>
 
-class Restaurant {
-    std::mutex* forks;
+#include "philosopher.h"
+#include "waiter.h"
 
-    Restaurant(int forksCount) {
-        forks = new std::mutex[forksCount];
-    }
-};
 
-class Waiter {
-    std::queue<int> waitingQueue;
-    Restaurant* restaurant;
-    std::mutex* waiterMutex;
-    Waiter(Restaurant* restaurant) {
-        this->restaurant = restaurant;
-        waiterMutex = new std::mutex();
-    }
-    bool Decide(int id) {
-    }
-};
+class Display {
+    std::vector<philosopher*> philosophers;
 
-class Philosopher {
-    int id;
-    Restaurant* restaurant;
-    Waiter* waiter;
-    bool eating;
-    std::mutex *left_fork;
-    std::mutex *right_fork;
-    std::mutex hungryMutex;
-    int hungryCount;
-    int sleepsec;
-
-    Philosopher(int id, Restaurant* restaurant, Waiter* waiter, std::mutex left_fork, std::mutex right_fork,int sleepsec) {
-        this->id = id;
-        this->restaurant = restaurant;
-        this->waiter = waiter;
-        this->left_fork = &left_fork;
-        this->right_fork = &right_fork;
-        this->sleepsec = sleepsec;
-        eating = false;
-    }
-
-    void Eat() {
-        if (!left_fork->try_lock()) {
-            std::cout <<"Philosopher " << id << "Tried to eat without left fork availible" << std::endl;
-            return;
-        }
-        if (!right_fork->try_lock()) {
-            std::cout <<"Philosopher " << id << "Tried to eat without right fork availible" << std::endl;
-            left_fork->unlock();
-            return;
-        }
-        eating = true;
-        std::this_thread::sleep_for(std::chrono::seconds(sleepsec));
-        hungryMutex.lock();
-        hungryCount = 0;
-        hungryMutex.unlock();
-        eating = false;
-        left_fork->unlock();
-        right_fork->unlock();
-    }
-
-    void think() {
-        std::this_thread::sleep_for(std::chrono::seconds(sleepsec/10));
-        hungryMutex.lock();
-        hungryCount++;
-        hungryMutex.unlock();
-    }
-
-    int check_hunger() {
-        hungryMutex.lock();
-        int count = hungryCount;
-        hungryMutex.unlock();
-        return count;
-    }
-
-    void try_eating() {
-
+public:
+    Display(std::vector<philosopher*> philosophers) {
+        this->philosophers = philosophers;
     }
 
     void Run() {
-        try_eating();
-        think();
+        while (true) {
+        std::cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << std::endl;
+        for (auto philosopher: philosophers) {
+            if (philosopher->is_eating()) {
+                std::cout << "Philosopher " << philosopher->get_id() << " is eating" << std::endl;
+            }
+            else {
+                std::cout << "Philosopher " << philosopher->get_id() << " didint eat for " << philosopher->check_hunger() << std::endl;
+            }
+        }
+        std::cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << std::endl;
+            //sleep for 1 second
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
     }
 };
 
 int main() {
+    //those are the variables needed to start the simulation that will later be used to create the objects
+    int sleepsec;
+    int number_of_philosophers;
+    //This part of code gets all the settings for simulation from users
+    std::cout << "How many philosophers should feast ?" << std::endl
+    << "number of philosophers = ";
+    std::cin >> number_of_philosophers;
+    std::cout << std::endl <<"There will be as many forks as there are filosophers" << std::endl;
+    std::cout << "How many seconds should each philosopher eat?" << std::endl;
+    std::cout << "sleepsec = ";
+    std::cin >> sleepsec;
+    std::cout << "Each philosopher will eat for " << sleepsec << " seconds." << std::endl;
 
+
+    //creating the objects
+    std::vector<philosopher*> philosophers;
+    std::mutex forks[number_of_philosophers];
+    waiter wtr = waiter();
+
+    for (int i = 0; i < number_of_philosophers; i++) {
+        philosophers.push_back(new philosopher(i, &wtr, &forks[i], &forks[(i + 1) % number_of_philosophers], sleepsec));
+        wtr.add_philosopher(philosophers[i]);
+    }
+
+    std::cout << "Simulation started";
+
+    //runnin each philosopher in a separate thread
+    std::vector<std::thread> threads;
+    for (auto philosopher: philosophers) {
+        threads.push_back(std::thread(&philosopher::run, philosopher));
+    }
+    //creating the display object
+    Display display(philosophers);
+    //running the display in a separate thread
+    std::thread displayThread(&Display::Run, &display);
+    //runnign while q is not pressed
+    while (std::cin.get() != 'q') {
+    }
 }
